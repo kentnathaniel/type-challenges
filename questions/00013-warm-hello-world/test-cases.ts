2b7497533f7c576efcea338794aa197ed0aa9667
kentnathaniel <kent.nathaniel2@gmail.com>
2024-04-04 00:46:49 +0700

    solution(warm): hello world

--- questions/00013-warm-hello-world/test-cases.ts
+++ questions/00013-warm-hello-world/test-cases.ts
@@ -1,5 +1,7 @@
 import type { Equal, Expect, NotAny } from '@type-challenges/utils'
 
+import type { HelloWorld } from './template'
+
 type cases = [
   Expect<NotAny<HelloWorld>>,
   Expect<Equal<HelloWorld, string>>,
