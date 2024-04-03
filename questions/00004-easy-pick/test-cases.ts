c4923a1c15bee0743d1abfe5ba6c82b407b1d9bf
kentnathaniel <kent.nathaniel2@gmail.com>
2024-04-04 01:18:09 +0700

    solution(easy): pick

--- questions/00004-easy-pick/test-cases.ts
+++ questions/00004-easy-pick/test-cases.ts
@@ -1,5 +1,14 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
+/*
+  K extends keyof T: To make sure all the keys in union T exist in K
+  P in K: Refer to  properties in union K, and we're doing mapping iteration for every properties
+*/
+
+type MyPick<T, K extends keyof T> = {
+  [P in K]: T[P]
+}
+
 type cases = [
   Expect<Equal<Expected1, MyPick<Todo, 'title'>>>,
   Expect<Equal<Expected2, MyPick<Todo, 'title' | 'completed'>>>,
@@ -7,7 +16,7 @@ type cases = [
   MyPick<Todo, 'title' | 'completed' | 'invalid'>,
 ]
 
-interface Todo {
+export interface Todo {
   title: string
   description: string
   completed: boolean
