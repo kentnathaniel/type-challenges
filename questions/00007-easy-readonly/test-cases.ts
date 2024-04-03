c5b4b13899fc4a1f12478cc243541d96e2f42448
kentnathaniel <kent.nathaniel2@gmail.com>
2024-04-04 01:21:39 +0700

    solution(easy): readonly

--- questions/00007-easy-readonly/test-cases.ts
+++ questions/00007-easy-readonly/test-cases.ts
@@ -1,5 +1,13 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
+/*
+  The same concept as creating pick, iterate over every properties and make them read-only
+*/
+
+type MyReadonly<T> = {
+  readonly [P in keyof T]: T[P]
+}
+
 type cases = [
   Expect<Equal<MyReadonly<Todo1>, Readonly<Todo1>>>,
 ]
