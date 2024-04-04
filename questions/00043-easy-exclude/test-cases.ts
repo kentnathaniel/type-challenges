7a16cc5780a43196e6b31d05ae315284523bc7d7
kentnathaniel <kent.nathaniel2@gmail.com>
2024-04-04 19:44:39 +0700

    solution(easy): exclude

--- questions/00043-easy-exclude/test-cases.ts
+++ questions/00043-easy-exclude/test-cases.ts
@@ -1,5 +1,12 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
+/*
+Applying distributive conditional types concept:
+typescriptlang.org/docs/handbook/2/conditional-types.html#distributive-conditional-types
+*/
+
+type MyExclude<T, U> = T extends U ? never : T
+
 type cases = [
   Expect<Equal<MyExclude<'a' | 'b' | 'c', 'a'>, 'b' | 'c'>>,
   Expect<Equal<MyExclude<'a' | 'b' | 'c', 'a' | 'b'>, 'c'>>,
